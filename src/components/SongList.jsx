85af867603eed39d4c7616f6f6c0f40fed705f54
andrewhb614 <andrew50412@gmail.com>
2023-05-28 03:11:33 +0800

    完成 ArtistList 初版

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -2,17 +2,15 @@ import styled from "@emotion/styled";
 
 import ArtistProfile from "./SongList/ArtistProfile";
 import TopTracks from "./SongList/TopTracks";
+import ArtistList from "./SongList/ArtistList";
 
 const Container = styled.nav`
   max-width: 24rem;
+  min-width: 18rem;
 	display: grid;
 	grid-template-rows: auto 1fr auto;
 	grid-row-gap: 1rem;
 	overflow: hidden;
-`;
-
-const Box = styled.div`
-	height: 6rem;
 `;
 
 const SongList = () => {
@@ -20,7 +18,7 @@ const SongList = () => {
 		<Container>
 			<ArtistProfile />
 			<TopTracks />
-			<Box />
+			<ArtistList />
 		</Container>
 	);
 };
--- src/components/SongList/ArtistList.jsx
+++ src/components/SongList/ArtistList.jsx
@@ -1,2 +1,69 @@
 import styled from "@emotion/styled";
 
+const Container = styled.div`
+	border-radius: 1rem;
+	display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(6rem, 1fr));
+	grid-column-gap: 0.5rem;
+  align-items: center;
+	overflow: hidden;
+	padding: 0.75rem;
+	background-color: var(--black-secondary);
+`;
+
+const Artist = styled.a`
+	display: grid;
+  grid-template-rows: auto 1fr;
+	align-items: center;
+  justify-content: center;
+	text-decoration: none;
+	&:hover {
+		color: var(--white-primary);
+		text-decoration: none;
+	}
+`;
+
+const ArtistImage = styled.img`
+	width: 6rem;
+	height: 6rem;
+	border-radius: 50%;
+`;
+
+const ArtistName = styled.div`
+	font-size: 1rem;
+	color: var(--white-secondary);
+  text-align: center;
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
+`;
+
+const artistData = [
+	{
+		img: "https://i.scdn.co/image/ab6761610000e5ebfc9d2abc85b6f4bef77f80ea",
+		name: "Pitbull",
+	},
+	{
+		img: "https://i.scdn.co/image/ab6761610000e5ebfc9d2abc85b6f4bef77f80ea",
+		name: "Pitbull",
+	},
+	{
+		img: "https://i.scdn.co/image/ab6761610000e5ebfc9d2abc85b6f4bef77f80ea",
+		name: "Pitbull",
+	},
+];
+
+const ArtistList = () => {
+	return (
+		<Container>
+			{artistData.map((artist, id) => (
+				<Artist id={id} href="#">
+					<ArtistImage src={artist.img} alt={artist.name} />
+					<ArtistName>{artist.name}</ArtistName>
+				</Artist>
+			))}
+		</Container>
+	);
+};
+
+export default ArtistList;
