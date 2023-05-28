2a52c285d9b5886c3e4f346514d826007954b1ae
andrewhb614 <andrew50412@gmail.com>
2023-05-29 03:07:52 +0800

    新增套件 react-spotify-api, 修改 App 完成 token 設置

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
+import { SpotifyApiContext } from "react-spotify-api";
 
+import passKeys from "./env";
 import TrackList from "./components/TrackList";
 import TrackInfo from "./components/TrackInfo";
+
+const { REACT_APP_CLIENT_ID, REACT_APP_CLIENT_SECRET } = passKeys();
 
 const Container = styled.div`
 	width: 90%;
@@ -15,12 +20,38 @@ const Container = styled.div`
 	background-color: var(--black-primary);
 `;
 
+const fetchToken = async (clientId, clientSecret) => {
+	const response = await fetch("https://accounts.spotify.com/api/token", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+		body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+	});
+
+	const data = await response.json();
+	// console.log("Access Token:", data.access_token);
+	return data.access_token;
+};
+
 const App = () => {
+	const [token, setToken] = useState("");
+
+	useEffect(() => {
+		const clientId = REACT_APP_CLIENT_ID;
+		const clientSecret = REACT_APP_CLIENT_SECRET;
+		fetchToken(clientId, clientSecret)
+			.then((token) => setToken(token))
+			.catch((err) => console.log("Error when accessing token:", err));
+	}, []);
+
 	return (
-		<Container>
-			<TrackList />
-			<TrackInfo />
-		</Container>
+		<SpotifyApiContext.Provider value={token}>
+			<Container>
+				<TrackList />
+				<TrackInfo />
+			</Container>
+		</SpotifyApiContext.Provider>
 	);
 };
 
