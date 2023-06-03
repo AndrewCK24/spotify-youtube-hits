3adf7949bd6ae6a48a917918bf348898068e77b2
andrewhb614 <andrew50412@gmail.com>
2023-06-04 02:45:28 +0800

    修改 FeatureSection 新增圖示

--- src/components/TrackInfo/FeatureSection.jsx
+++ src/components/TrackInfo/FeatureSection.jsx
@@ -5,6 +5,10 @@ import currentArtistDataState from "../../recoil/atoms/currentArtistDataState";
 import currentTrackDataState from "../../recoil/atoms/currentTrackDataState";
 import currentTrackKeyState from "../../recoil/atoms/currentTrackKeyState";
 import { intFormatter } from "../../utils/numberFormatter";
+import { AiFillLike } from "react-icons/ai";
+import { MdInsertComment } from "react-icons/md";
+import { FaItunesNote } from "react-icons/fa";
+import { TbActivityHeartbeat } from "react-icons/tb";
 
 const Container = styled.section`
 	grid-row: 3 / 4;
@@ -27,6 +31,11 @@ const FeatureContainer = styled.div`
 `;
 
 const Title = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: left;
+	align-items: center;
+	gap: 0.25rem;
 	font-size: 1rem;
 	font-weight: 400;
 	position: relative;
@@ -73,19 +82,19 @@ const FeatureSection = () => {
 	return (
 		<Container>
 			<FeatureContainer>
-				<Title>YouTube Likes</Title>
+				<Title>YouTube Likes<AiFillLike /></Title>
 				<Number>{intFormatter(popularity?.youtubeLikes)}</Number>
 			</FeatureContainer>
 			<FeatureContainer>
-				<Title>YouTube Comments</Title>
+				<Title>YouTube Comments<MdInsertComment /></Title>
 				<Number>{intFormatter(popularity?.youtubeComments)}</Number>
 			</FeatureContainer>
 			<FeatureContainer>
-				<Title>Pitch</Title>
+				<Title>Pitch<FaItunesNote /></Title>
 				<Number>{pitchConverter(features?.key)}</Number>
 			</FeatureContainer>
 			<FeatureContainer>
-				<Title>Tempo (bpm)</Title>
+				<Title>Tempo (bpm)<TbActivityHeartbeat /></Title>
 				<Number>{intFormatter(features?.tempo)}</Number>
 			</FeatureContainer>
 		</Container>
