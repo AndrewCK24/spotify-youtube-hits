fbace46b03f757a44e23e3ac401427a792ae7139
andrewhb614 <andrew50412@gmail.com>
2023-06-03 22:09:15 +0800

    修改 SearchBar 完成清除 input 資料功能

--- src/components/TrackList/SearchBar.jsx
+++ src/components/TrackList/SearchBar.jsx
@@ -1,27 +1,58 @@
+import { useEffect, useState, useRef } from "react";
 import styled from "@emotion/styled";
 
+import { RxCross1 } from "react-icons/rx";
+
+const Container = styled.div`
+	font-size: 1.25rem;
+	border-radius: 1rem;
+	background-color: ${(props) =>
+		props.isFocus ? "var(--white-primary)" : "var(--gray-secondary)"};
+	display: grid;
+	grid-template-columns: 1fr auto;
+	align-items: center;
+	padding: 0 0.5rem;
+	&:hover {
+		background-color: var(--white-primary);
+	}
+`;
+
 const SearchInput = styled.input`
-	background-color: var(--gray-secondary);
+	background-color: transparent;
+	font-size: 1rem;
 	border: none;
 	border-radius: 1rem;
 	padding: 0.75rem;
 	color: var(--black-primary);
-	&:hover,
 	&:focus {
-		background-color: var(--white-primary);
 		outline: none;
 	}
 `;
 
-const SearchBar = ({handleChange, setFocus}) => {
+const SearchBar = ({ handleChange, focus, setFocus }) => {
+	const [containerFocus, setContainerFocus] = useState(focus);
+	const inputRef = useRef(null);
+	useEffect(() => {
+		setContainerFocus(focus);
+	}, [focus]);
+	const handleClearInput = () => {
+		setFocus(false);
+		if (inputRef.current) {
+			inputRef.current.value = "";
+		}
+	};
+
 	return (
+		<Container isFocus={containerFocus}>
 			<SearchInput
+				ref={inputRef}
 				type="text"
 				placeholder="Search for an artist"
 				onChange={handleChange}
 				onFocus={() => setFocus(true)}
-        // onBlur={() => setFocus(false)}
 			/>
+			{focus && <RxCross1 onClick={handleClearInput} />}
+		</Container>
 	);
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import App from "./App";
 import "normalize.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<HashRouter>
+	<BrowserRouter>
 		<RecoilRoot>
 			<App />
 		</RecoilRoot>
-	</HashRouter>
+	</BrowserRouter>
 );
