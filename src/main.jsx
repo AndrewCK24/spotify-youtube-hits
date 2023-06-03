232b9405ed0e5f22410bdb272b9bb33c1eeda422
andrewhb614 <andrew50412@gmail.com>
2023-06-04 03:59:36 +0800

    修改 main 移除 react-router-dom 的引入

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import App from "./App";
 import "normalize.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<BrowserRouter>
 		<RecoilRoot>
 			<App />
 		</RecoilRoot>
-	</BrowserRouter>
 );
